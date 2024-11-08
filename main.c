67de703609727701109682769050428520f64793
jokole3 <paesdoorball@mail.ru>
2024-11-08 20:09:48 +0500

    Все правильно!

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main(int argc, char **argv)
 { 
     char first[255], last[255]; 
     askname(first, last); 
+    askname(first); 
+    askname(last); 
     printf("Hello, %s %s!\n", first, last); 
     return 0; 
 } 
